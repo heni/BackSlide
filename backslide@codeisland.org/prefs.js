d024e68d83516bb80dcf67f2d04e8c59532c408f
Lukas Knuth <mr.luke.187@googlemail.com>
2013-02-11 17:55:06 +0100

    The images are now displayed in a grid. See #19

--- backslide@codeisland.org/prefs.js
+++ backslide@codeisland.org/prefs.js
@@ -19,6 +19,8 @@ let settings;
 let list_selection;
 let visible = false;
 const IMAGE_REGEX = /^image\/\w+$/i;
+const PIXBUF_COL = 0;
+const PATH_COL = 1;
 /**
  * Called right after the file was loaded.
  */
@@ -27,27 +29,26 @@ function init(){
     settings = new Pref.Settings();
 }
 
-// TODO Keep numbering consistent (with row-changes).
-
-function addFileEntry(model, path, number){
+function addFileEntry(model, path){
     // Load and scale the image from the given path:
     let image;
     try {
-        image = Pixbuf.Pixbuf.new_from_file_at_scale(path, 200, 100, true);
+        image = Pixbuf.Pixbuf.new_from_file_at_scale(path, 250, -1, true);
     } catch (e){
         // Image could not be loaded. Invalid path.
         /*
             It's okay to do nothing here, when the list is stored, the invalid image will not be
             stored with the rest, so it's practically gone.
         */
+        print(e);
     }
     if (image === undefined) return;
     // Append to the list:
     let iterator = model.append();
-    model.set(iterator, [0,1,2], [number, image, path]);
+    model.set(iterator, [PIXBUF_COL,PATH_COL], [image, path]);
 }
 
-function addDirectory(model, path, number){
+function addDirectory(model, path){
     let dir = Gio.file_new_for_path(path);
     if (dir.query_file_type(Gio.FileQueryInfoFlags.NONE, null) != Gio.FileType.DIRECTORY){
         // Not a valid directory!
@@ -60,11 +61,11 @@ function addDirectory(model, path, number){
         if (info.get_file_type() == Gio.FileType.REGULAR && info.get_is_hidden() == false){
             // Check if it's an image:
             if (info.get_content_type().match(IMAGE_REGEX) != null){
-                addFileEntry(model, path+"/"+info.get_name(), number);
+                addFileEntry(model, path + "/" + info.get_name());
             }
         } else if (info.get_file_type() == Gio.FileType.DIRECTORY && !info.get_is_hidden()){
             // Recursive search:
-            addDirectory(model, path+"/"+info.get_name(), number);
+            addDirectory(model, path + "/" + info.get_name());
         }
     }
 }
@@ -75,47 +76,40 @@ function addDirectory(model, path, number){
  */
 function buildPrefsWidget(){
     let frame = new Gtk.Box({
-        orientation: Gtk.Orientation.HORIZONTAL
+        orientation: Gtk.Orientation.HORIZONTAL,
+        width_request: 800,
+        height_request: 600
     });
 
     // The model for the tree:
     // See (and next page): http://scentric.net/tutorial/sec-treeviewcol-renderer.html
-    let list_model = new Gtk.ListStore();
-    list_model.set_column_types([GObject.TYPE_INT, Pixbuf.Pixbuf, GObject.TYPE_STRING]); // See http://blogs.gnome.org/danni/2012/03/29/gtk-liststores-and-clutter-listmodels-in-javascriptgjs/
+    let grid_model = new Gtk.ListStore();
+    grid_model.set_column_types([Pixbuf.Pixbuf, GObject.TYPE_STRING]); // See http://blogs.gnome.org/danni/2012/03/29/gtk-liststores-and-clutter-listmodels-in-javascriptgjs/
     // The String-column is not visible and only used for storing the path to the pixbuf (no way of finding out later).
 
-    // The tree itself:
-    let image_tree = new Gtk.TreeView({
+    // TODO Make grid fit... look and feel, etc
+    // TODO Make selection mode MULTIPLE and allow deleting multiple items (only delete??)
+    // TODO Make new screenshot and update the README
+    // The Image-Grid:
+    let image_grid = new Gtk.IconView({
+        spacing: 2,
+        columns: 3,
         expand: true,
-        model: list_model
+        item_padding: 2,
+        selection_mode: Gtk.SelectionMode.SINGLE,
+        model: grid_model,
+        pixbuf_column: PIXBUF_COL,
+        text_column: -1
     });
-    list_selection = image_tree.get_selection();
-    // The number-column
-    let number_col = new Gtk.TreeViewColumn({
-        title: "#"
-    });
-    image_tree.append_column(number_col);
-    let number_renderer = new Gtk.CellRendererText();
-    number_col.pack_start(number_renderer, false);
-    number_col.add_attribute(number_renderer, "text", 0);
 
-    // The image-column:
-    let image_col = new Gtk.TreeViewColumn({
-        title: _("Wallpaper")
-    });
-    image_tree.append_column(image_col);
-    let image_renderer = new Gtk.CellRendererPixbuf();
-    image_col.pack_start(image_renderer, false);
-    image_col.add_attribute(image_renderer, "pixbuf", 1);
-
-    let tree_scroll = new Gtk.ScrolledWindow();
-    tree_scroll.add(image_tree);
-    frame.add(tree_scroll);
+    let grid_scroll = new Gtk.ScrolledWindow();
+    grid_scroll.add(image_grid);
+    frame.add(grid_scroll);
 
     // Fill the Model:
     let image_list = settings.getImageList();
     for (let i = 0; i < image_list.length; i++){
-        addFileEntry(list_model, image_list[i], i+1);
+        addFileEntry(grid_model, image_list[i]);
     }
     // Pre-populate the list with default wallpapers:
     if (image_list.length <= 0){
@@ -125,7 +119,7 @@ function buildPrefsWidget(){
             stores it's images...
             Anyways, if it's not, the search will gracefully die in peace.
          */
-        addDirectory(list_model, "/usr/share/backgrounds/gnome/", 0);
+        addDirectory(grid_model, "/usr/share/backgrounds/gnome");
     }
 
     // Toolbar to the right:
@@ -141,13 +135,16 @@ function buildPrefsWidget(){
         })
     });
     move_up_button.connect('clicked', function(){
-        let [ isSelected, model, iterator_current ] = list_selection.get_selected();
-        if (isSelected){
-            let iterator_up = iterator_current.copy();
-            list_model.iter_previous(iterator_current);
-            list_model.swap(iterator_current, iterator_up);
-            // Manually fire the "row-changed"-event so "button_state_callback" gets triggered!
-            list_model.row_changed(list_model.get_path(iterator_current), iterator_current);
+        let selection = image_grid.get_selected_items();
+        if (selection.length != 0){
+            let [success, iterator_current] = grid_model.get_iter(selection[0]);
+            if (success){
+                let iterator_up = iterator_current.copy();
+                grid_model.iter_previous(iterator_current);
+                grid_model.swap(iterator_current, iterator_up);
+                // Manually fire the "row-changed"-event so "button_state_callback" gets triggered!
+                grid_model.row_changed(selection[0], iterator_current);
+            }
         }
     });
     toolbar.add(move_up_button);
@@ -160,13 +157,16 @@ function buildPrefsWidget(){
         margin_bottom: 4
     });
     move_down_button.connect('clicked', function(){
-        let [ isSelected, model, iterator_current ] = list_selection.get_selected();
-        if (isSelected){
-            let iterator_down = iterator_current.copy();
-            list_model.iter_next(iterator_current);
-            list_model.swap(iterator_current, iterator_down);
-            // Manually fire the "row-changed"-event so "button_state_callback" gets triggered!
-            list_model.row_changed(list_model.get_path(iterator_current), iterator_current);
+        let selection = image_grid.get_selected_items();
+        if (selection.length != 0){
+            let [success, iterator_current] = grid_model.get_iter(selection[0]);
+            if (success){
+                let iterator_down = iterator_current.copy();
+                grid_model.iter_next(iterator_current);
+                grid_model.swap(iterator_current, iterator_down);
+                // Manually fire the "row-changed"-event so "button_state_callback" gets triggered!
+                grid_model.row_changed(selection[0], iterator_current);
+            }
         }
     });
     toolbar.add(move_down_button);
@@ -198,9 +198,9 @@ function buildPrefsWidget(){
                 let type = path.query_file_type(Gio.FileQueryInfoFlags.NONE, null);
                 // Check whether a directory or a file was chosen:
                 if (type == Gio.FileType.REGULAR){
-                    addFileEntry(list_model, files[i], i);
+                    addFileEntry(grid_model, files[i]);
                 } else if (type == Gio.FileType.DIRECTORY){
-                    addDirectory(list_model, files[i], i);
+                    addDirectory(grid_model, files[i]);
                 }
             }
         }
@@ -215,9 +215,13 @@ function buildPrefsWidget(){
         })
     });
     remove_button.connect('clicked', function(){
-        let [ isSelected, model, iterator ] = list_selection.get_selected();
-        if (isSelected){
-            list_model.remove(iterator);
+        let selection = image_grid.get_selected_items();
+        if (selection.length != 0){
+            let [success, iterator_current] = grid_model.get_iter(selection[0]);
+            if (success){
+                grid_model.remove(iterator_current);
+                // TODO Move selection to next/previous item ?
+            }
         }
     });
     toolbar.add(remove_button);
@@ -225,33 +229,35 @@ function buildPrefsWidget(){
     // Check if we can move up/down and deactivate buttons if not.
     let button_state_callback = function(){
         // Check if we have data:
-        if (list_model.iter_n_children(null) <= 1){
+        if (grid_model.iter_n_children(null) <= 1){
             move_up_button.set_sensitive(false);
             move_down_button.set_sensitive(false);
             return;
         }
         // Otherwise, we have data:
-        let [ isSelected, model, iterator ] = list_selection.get_selected();
-        // Something needs to be selected:
-        if (!isSelected) return;
-
-        // We have a selection and data:
-        if (list_model.iter_next(iterator.copy()) === false){
-            // We're at the bottom:
-            move_up_button.set_sensitive(true);
-            move_down_button.set_sensitive(false);
-        } else if (list_model.iter_previous(iterator.copy()) === false){
-            // We're at the top:
-            move_up_button.set_sensitive(false);
-            move_down_button.set_sensitive(true);
-        } else {
-            // We're in the middle, can move up and down:
-            move_up_button.set_sensitive(true);
-            move_down_button.set_sensitive(true);
+        let selection = image_grid.get_selected_items();
+        if (selection.length != 0){
+            let [success, iterator] = grid_model.get_iter(selection[0]);
+            if (success){
+                // We have a selection and data:
+                if (grid_model.iter_next(iterator.copy()) === false){
+                    // We're at the bottom:
+                    move_up_button.set_sensitive(true);
+                    move_down_button.set_sensitive(false);
+                } else if (grid_model.iter_previous(iterator.copy()) === false){
+                    // We're at the top:
+                    move_up_button.set_sensitive(false);
+                    move_down_button.set_sensitive(true);
+                } else {
+                    // We're in the middle, can move up and down:
+                    move_up_button.set_sensitive(true);
+                    move_down_button.set_sensitive(true);
+                }
+            }
         }
     };
-    list_selection.connect('changed', button_state_callback);
-    list_model.connect('row-changed', button_state_callback);
+    image_grid.connect('selection-changed', button_state_callback);
+    grid_model.connect('row-changed', button_state_callback);
 
     // Store the changes in the settings, when the window is closed or the settings change:
     // Workaround, see https://bugzilla.gnome.org/show_bug.cgi?id=687510
@@ -262,13 +268,13 @@ function buildPrefsWidget(){
         }
         visible = false;
         // Save the list:
-        let [ success, iterator ] = list_model.get_iter_first();
+        let [ success, iterator ] = grid_model.get_iter_first();
         let list = [];
         if (success){
             do {
-                let img_path = list_model.get_value(iterator, 2);
+                let img_path = grid_model.get_value(iterator, PATH_COL);
                 list.push(img_path);
-            } while (list_model.iter_next(iterator));
+            } while (grid_model.iter_next(iterator));
         }
         settings.setImageList(list);
     });
