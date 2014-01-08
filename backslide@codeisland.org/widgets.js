dbc881085022e62f10fd4869593c560affc92886
Wolfgang Gaar <gaarwolfgang@gmail.com>
2014-01-08 18:26:48 +0100

    fixed delay slider

--- backslide@codeisland.org/widgets.js
+++ backslide@codeisland.org/widgets.js
@@ -479,17 +479,13 @@ const SliderItem = new Lang.Class({
     Name: 'SliderItem',
     Extends: PopupMenu.PopupBaseMenuItem,
 
-    _init: function(value, label, icon) {
+    _init: function(value) {
         this.parent();
 
         this._box = new St.Table({style_class: 'slider-item'});
 
-        this._icon = new St.Icon({style_class: 'menu-icon', icon_name: icon + '-symbolic'});
         this._slider = new Slider.Slider(value);
-        this._label = new St.Label({text: label});
 
-        this._box.add(this._icon, {row: 0, col: 0, x_expand: false});
-        this._box.add(this._label, {row: 0, col: 1, x_expand: false});
         this._box.add(this._slider.actor, {row: 0, col: 2, x_expand: true});
         this.actor.add(this._box, {span: -1, expand: true});
     },
@@ -504,11 +500,6 @@ const SliderItem = new Lang.Class({
 
     setIcon: function(icon) {
         this._icon.icon_name = icon + '-symbolic';
-    },
-
-    setLabel: function(text) {
-        if (this.clutter_text)
-            this.text = text;
     },
 
     connect: function(signal, callback) {
@@ -603,6 +594,6 @@ const LabelWidget = new Lang.Class({
      * @param text the new text.
      */
     setText: function(text){
-        this._label.text = text.toString();
+        this._label.text = text;
     }
 });
