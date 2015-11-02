2f24b1f447f60346eccc5c8ec9cfa7007b4cc0b4
Paolo Inaudi <p91paul@gmail.com>
2015-11-02 08:03:58 +0100

    Adapt for Gnome Shell 3.18

--- backslide@codeisland.org/widgets.js
+++ backslide@codeisland.org/widgets.js
@@ -475,12 +475,14 @@ const SliderItem = new Lang.Class({
 
     _init: function(value) {
         this.parent();
-
-        this._box = new St.Table({style_class: 'slider-item'});
+		var layout = new Clutter.TableLayout();
+        this._box = new St.Widget({
+							style_class: 'slider-item',
+							layout_manager: layout});
 
         this._slider = new Slider.Slider(value);
 
-        this._box.add(this._slider.actor, {row: 0, col: 2, x_expand: true});
+        layout.pack(this._slider.actor, 2, 0);
         this.actor.add(this._box, {span: -1, expand: true});
     },
 
