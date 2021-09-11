79bb5e3971db2431b4899fa1bd4350d443b86d58
Paolo Inaudi <p91paul@gmail.com>
2021-09-11 15:59:00 +0200

    Exclude .po files from upload archive

--- buildforupload.sh
+++ buildforupload.sh
@@ -2,4 +2,4 @@
 rm -rf backslide@codeisland.org.zip
 cd backslide@codeisland.org/
 glib-compile-schemas schemas/
-zip -r ../backslide@codeisland.org.zip *
+zip -r ../backslide@codeisland.org.zip * -x '*.po'
